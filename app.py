a8c06f008df96d1459a9edc3a7cac3c28a22e836
Bran <brandonsultana1@gmail.com>
2022-01-06 15:33:05 -0500

    Local SQLite Database

--- app.py
+++ app.py
@@ -2,13 +2,78 @@ from flask import Flask, redirect, url_for, request, render_template
 import json
 import boto3
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-client = boto3.client('s3', aws_access_key_id=os.environ["ACCESSKEY"], aws_secret_access_key=os.environ["SECRETKEY"])
 
-print(os.environ["Environment"])
+try:
+    client = boto3.client('s3', aws_access_key_id=os.environ["ACCESSKEY"], aws_secret_access_key=os.environ["SECRETKEY"])
+except KeyError:
+    client = None
 
+class Character(db.Model):
+    value=db.Column(db.String(80), primary_key=True)
+    name=db.Column(db.String(80))
+    series=db.Column(db.String(80))
+    number = db.Column(db.String(80))
+    version = db.Column(db.String(80))
+    id = db.Column(db.Integer)
+    completed = db.Column(db.Boolean)
+    moves = db.relationship('Move', backref="Character", lazy=True)
+
+    def __repr__(self):
+        return self.name
+
+class Move(db.Model):
+    value=db.Column(db.String(80), primary_key=True)
+    name=db.Column(db.String(80))
+    character=db.Column(db.String(80), db.ForeignKey('character.value'))
+    frames=db.Column(db.Integer)
+    faf=db.Column(db.Integer)
+    notes=db.Column(db.String(200))
+    hitboxes=db.relationship('Hitbox', backref="Move", lazy=True)
+
+    def __repr__(self):
+        return self.name
+
+class Hitbox(db.Model):
+    value=db.Column(db.String(80), primary_key=True)
+    move=db.Column(db.String(80), db.ForeignKey('move.value'))
+    color=db.Column(db.String(80))
+    notes=db.Column(db.String(200))
+    data=db.Column(db.JSON)
+    frames=db.Column(db.JSON)
+
+class Grab(db.Model):
+    value=db.Column(db.String(80), primary_key=True)
+    move=db.Column(db.String(80), db.ForeignKey('move.value'))
+    color=db.Column(db.String(80))
+    notes=db.Column(db.String(200))
+    data=db.Column(db.JSON)
+    frames=db.Column(db.JSON)
+
+class Throw(db.Model):
+    value=db.Column(db.String(80), primary_key=True)
+    move=db.Column(db.String(80), db.ForeignKey('move.value'))
+    color=db.Column(db.String(80))
+    notes=db.Column(db.String(200))
+    data=db.Column(db.JSON)
+    frames=db.Column(db.JSON)
+
+class Hurtbox(db.Model):
+    value=db.Column(db.String(80), primary_key=True)
+    move=db.Column(db.String(80), db.ForeignKey('move.value'))
+    color=db.Column(db.String(80))
+    notes=db.Column(db.String(200))
+    bone=db.Column(db.String(200))
+    frames=db.Column(db.JSON)
+    hp=db.Column(db.String(80))
+    type=db.Column(db.String(80))
+
+#db.create_all()
 def getCharacterFile(character):
     
     try:
@@ -47,6 +112,9 @@ def getMove(character, move):
 
 @app.route('/api/images/<string:character>/<string:move>', methods=["GET"])
 def getImages(character, move):
+
+    if client == None:
+        return {"Error": "Error requesting S3 Credentials"}
 
     data = getCharacterFile(character)
 
